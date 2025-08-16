b1318c7ebe06e03140d5ee24ba52a33bfb08d253
Agus9217 <justinibarrola92@gmail.com>
2025-08-16 18:40:03 -0300

    second commit

--- src/assets/font/index.ts
+++
@@ -1,5 +0,0 @@
-import { Barlow_Condensed, Bebas_Neue, Lora } from 'next/font/google'
-
-export const barlow = Barlow_Condensed({ subsets: ['latin'], weight: ['400', '600', '700', '800', '900'] })
-export const bebas = Bebas_Neue({ subsets: ['latin'], weight: '400' })
-export const lora = Lora({ subsets: ['latin'] })
